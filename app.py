88229a9ef6beb14d1177a67e5ff51389272bd6f6
huyyenbang <34955676+huyyenbang@users.noreply.github.com>
2025-08-04 08:21:05 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, Response, request, redirect
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch(path):
+    if request.method == 'HEAD':
+        resp = Response("")
+        resp.headers['Content-Type'] = 'text/x-component'
+        return resp
+    return redirect('https://example.com')
